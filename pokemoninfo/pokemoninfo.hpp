92653494307d9714fc76faba3edfa633a5a85b28
Franco Needleman <franeedle@gmail.com>
2025-05-30 13:34:31 -0300

    dub pokemoninfohpp

---
+++ pokemoninfo/pokemoninfo.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class pokemoninfo {
+private:
+    string type;
+    string descripcion;
+    void* ataquesDisponiblesPorNivel;
+    void* experienciaProximoNivel;
+};
+
